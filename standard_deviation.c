0a92bf35ca068148f41d8825992b300174c24564
Cory Buecker <email@corybuecker.com>
2012-01-08 12:44:42 -0600

    Refined method for speed

--- standard_deviation.c
+++ standard_deviation.c
@@ -1,38 +1,37 @@
 #include "ruby.h"
 #include "math.h"
-double total;
-double mean;
-double total_distance_from_mean;
 
-VALUE sum(VALUE self)
-{
-  total = total + NUM2DBL(self);
-  return Qnil;
-}
-VALUE distance_from_mean(VALUE self)
-{
-  total_distance_from_mean = total_distance_from_mean + pow((NUM2DBL(self) - mean),2);
-  return Qnil;
-}
 double get_mean(VALUE ary)
 {
-  rb_iterate(rb_each, ary, sum, 0); 
-  if(RARRAY_LEN(ary) == 0){
+  VALUE* array_pointer = RARRAY_PTR(ary);
+  double total = 0;
+  int i = 0;
+  long array_length = RARRAY_LEN(ary);
+  while (i < array_length){
+    total += NUM2DBL(array_pointer[i++]);
+  }
+  if(array_length == 0){
     return 0.0;
   } else {
-    return total/RARRAY_LEN(ary);
+    return total / array_length;
   }
 }
 
-static VALUE standard_deviation(VALUE self)
+VALUE standard_deviation(VALUE ary)
 {
-  mean = get_mean(self);
-  total_distance_from_mean = 0;
-  rb_iterate(rb_each, self, distance_from_mean, 0);
-  if (RARRAY_LEN(self)==0){
+  double mean = get_mean(ary);
+  double total_distance_from_mean = 0;
+  VALUE* array_pointer = RARRAY_PTR(ary);
+  int i = 0;
+  long array_length = RARRAY_LEN(ary);
+  while (i < array_length){
+    total_distance_from_mean = total_distance_from_mean + pow((NUM2DBL(array_pointer[i++]) - mean),2);
+  }
+  
+  if (array_length == 0){
     return rb_float_new(0);
   } else {
-    return rb_float_new(sqrt((total_distance_from_mean/RARRAY_LEN(self))));
+    return rb_float_new(sqrt((total_distance_from_mean/array_length)));
   }
     
 }
@@ -43,6 +42,5 @@ static VALUE standard_deviation(VALUE self)
 
 void Init_StandardDeviation()
 {
-  total = 0;
   rb_define_method(rb_cArray, "standard_deviation", standard_deviation, 0);
 }
--- standard_deviation.rb
+++ standard_deviation.rb
@@ -1,7 +1,25 @@
 require 'StandardDeviation'
+require 'benchmark'
+
+class Array
+  def sum
+    inject(0.0) { |result, el| result + el }
+  end
+
+  def mean 
+    sum / size
+  end
+end
 
 
-
-r = [3,4,1,42,14,1]
-
-p r.standard_deviation
+Benchmark.bm do |x|
+  x.report("C Extension") {
+   (1..5000).to_a.standard_deviation
+  }
+  x.report("Native Ruby"){
+    a = (1..5000).to_a
+    avg = a.mean
+    distance = a.inject(0.0){|result, element| result + (element - avg)**2}
+    Math.sqrt(distance/a.length)
+  }
+end
