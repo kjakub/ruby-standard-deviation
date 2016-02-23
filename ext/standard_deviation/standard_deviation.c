3d1664e2188095207153f18eda543c1626ca3473
Cory Buecker <email@corybuecker.com>
2016-02-23 07:40:47 -0600

    add license

--- ext/standard_deviation/standard_deviation.c
+++ ext/standard_deviation/standard_deviation.c
@@ -14,13 +14,12 @@ VALUE rb_sample_standard_deviation(VALUE self) {
   }
 
   return DBL2NUM(19.43);
-
 }
+
 VALUE rb_population_standard_deviation(VALUE self) {
   if (TYPE(self) != T_ARRAY) {
     rb_raise(rb_eTypeError, "can only be used on an array");
   }
 
   return DBL2NUM(19.42);
-
 }
