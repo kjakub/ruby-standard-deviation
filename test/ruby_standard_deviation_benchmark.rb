e08fd8174dd47d4e03983a7d6116316c3f28aa9d
Cory Buecker <email@corybuecker.com>
2016-02-24 06:15:40 -0600

    faster tests

--- test/ruby_standard_deviation_benchmark.rb
+++ test/ruby_standard_deviation_benchmark.rb
@@ -4,13 +4,13 @@ require "test_helper"
 class RubyStandardDeviationBenchmark < Minitest::Benchmark
   def bench_native_extension
     assert_performance_constant 0.99 do |input|
-      (1..1000000).to_a.stdev
+      (1..10000).to_a.stdev
     end
   end
 
   def bench_pure_ruby
     assert_performance_constant 0.99 do |input|
-      array = (1..1000000).to_a
+      array = (1..10000).to_a
       total = array.inject(0) {|total, element| total + element }
       mean = total.to_f / array.size
       total_distance_from_mean = array.inject(0){|total, element| total + (element - mean)**2 }
